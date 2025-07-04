febbe0eaed6324afa6958360bc434b16fe21602a
yoquienmas <maria_dniel@yahoo.com.ar>
2025-07-04 19:53:26 -0300

    Tercera commit: baja de libros

--- src/controller.js
+++ src/controller.js
@@ -15,6 +15,13 @@ class LibroController {
     );
       res.json({ "Id insertado": result.insertId });
     }
+
+    async delete(req, res) {
+    const libro = req.body;
+      const [result] = await pool.query(`DELETE FROM Libros WHERE ISBN=(?)`, [libro.ISBN]);
+      res.json({ "Registros eliminados": result.affectedRows });
+  }
+
 }
 
 export const libro = new LibroController();
